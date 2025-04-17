c05cbb88993998491f0b637c5a358258d040de93
mrtvtb <mrtvtb@gmail.com>
2025-04-17 16:24:05 +0200

    add ui

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
-print("Hello World")
+from PyQt5.QtWidgets import (
+   QApplication, QWidget,
+   QFileDialog,
+   QLabel, QPushButton, QListWidget,
+   QHBoxLayout, QVBoxLayout
+)
+
+app = QApplication([])
+win = QWidget()
+win.setWindowTitle("Easy Editor")
+win.resize(700,500)
+
+lb_image = QLabel("Картинка")
+btn_dir = QPushButton("Папка")
+lw_files = QListWidget()
+
+btn_left = QPushButton("Вліво") 
+btn_right = QPushButton("Вправо")
+btn_flip = QPushButton("Дзеркало")
+btn_sharp = QPushButton("Різкість")
+btn_bw = QPushButton("Ч/Б")
+
+row = QHBoxLayout()
+col1 = QVBoxLayout()
+col2 = QVBoxLayout()
+col1.addWidget(btn_dir)
+col1.addWidget(lw_files)
+col2.addWidget(lb_image, 95)
+row_tools = QHBoxLayout()
+row_tools.addWidget(btn_left)
+row_tools.addWidget(btn_right)
+row_tools.addWidget(btn_flip)
+row_tools.addWidget(btn_sharp)
+row_tools.addWidget(btn_bw)
+col2.addLayout(row_tools)
+
+row.addLayout(col1, 20)
+row.addLayout(col2, 80)
+
+win.setLayout(row)
+
+win.show()
+app.exec_()
